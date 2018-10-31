0abaa5f5af8396575a99835036cf8f0da18f960b
gkucmierz <gkucmierz@gmail.com>
2018-10-31 19:13:31 +0100

    fixed error handling

--- bin/index.js
+++ bin/index.js
@@ -15,24 +15,31 @@ const request = require('request');
 
 let addresses = (fs.readFileSync(flags.i)+'').match(/0x[0-9a-f]{40}/ig);
 
-
 // console log balances
 (function loop() {
 
   let chunk = addresses.splice(0, 20);
-
-  request(API(chunk.join(',')), function (error, response, body) {
+  let joined = chunk.join(',').toLowerCase();
+  
+  request(API(joined), function (error, response, body) {
     if (error) {
       console.error(error);
     }
     
-    JSON.parse(body).result.map(acc => {
-      console.log(`${acc.account}  ${acc.balance}`);
-    });
+    let parsed = JSON.parse(body);
+    if (parsed.status === '0') {
+      console.error(parsed.result);
+      console.error(joined);
+    } else {
+      JSON.parse(body).result.map(acc => {
+        console.log(`${acc.account}  ${acc.balance}`);
+      });
 
-    if (addresses.length) {
-      loop();
+      if (addresses.length) {
+        loop();
+      }      
     }
+
   });
 
 }());
