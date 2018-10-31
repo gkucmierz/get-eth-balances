5bb6b8d49170254b251077e146bb0fb38ce8924f
gkucmierz <gkucmierz@gmail.com>
2018-10-31 18:15:13 +0100

    test args

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,12 @@
+#!/usr/bin/env node
 
+import args from 'args';
+ 
+args
+  .option('port', 'The port on which the app will be running', 3000)
+  .option('reload', 'Enable/disable livereloading')
+  .command('serve', 'Serve your static site', ['s']);
+ 
+const flags = args.parse(process.argv);
 
-console.log('test');
+console.log(flags);
