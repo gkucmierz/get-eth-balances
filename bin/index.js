88c7b6774bf54edc995487a801b31739665f057c
gkucmierz <gkucmierz@gmail.com>
2018-10-31 18:57:33 +0100

    finished

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,40 @@
 #!/usr/bin/env node
 
-import args from 'args';
+const args = require('args');
  
 args
-  .option('port', 'The port on which the app will be running', 3000)
-  .option('reload', 'Enable/disable livereloading')
-  .command('serve', 'Serve your static site', ['s']);
+  .option('input', 'The file with ETH addresses, that will be processed');
  
 const flags = args.parse(process.argv);
 
-console.log(flags);
+const API = addr => `https://api.etherscan.io/api?module=account&action=balancemulti&address=${addr}&tag=latest&apikey=YourApiKeyToken`;
+
+const fs = require('fs');
+const request = require('request');
+
+
+let addresses = (fs.readFileSync(flags.i)+'').match(/0x[0-9a-f]{40}/ig);
+
+
+// console log balances
+(function loop() {
+
+  let chunk = addresses.splice(0, 20);
+
+  request(API(chunk.join(',')), function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    
+    JSON.parse(body).result.map(acc => {
+      console.log(`${acc.account}  ${acc.balance}`);
+    });
+
+    if (addresses.length) {
+      loop();
+    }
+  });
+
+}());
+
+
